f845ff16b81f202eb96a423a40ad8b12f6330956
Allan Paul (s3849758) <93415378+Humanerer@users.noreply.github.com>
2022-01-19 18:16:17 +1100

    added methods to test other methods

--- SimpleTD.java
+++ SimpleTD.java
@@ -1,5 +1,18 @@
+import java.util.LinkedList;
+
 class SimpleTD {
     public static void main(String[] args) {
-        
+        LinkedList<CoordVector> path = new LinkedList<CoordVector>();
+        for (int x = 0; x < 32; x++) {
+            path.add(new CoordVector(x,4));
+        }
+
+
+
+        Board board = new Board(32,18, path);
+        board.printBoard();
+        System.out.println();
+        board.placeTower(new Tower(),4,5);
+        board.printBoard();
     }
 }
