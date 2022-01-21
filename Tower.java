b76c74a2e78012a8c341bb85b03211e63c6e1655
Allan Paul (s3849758) <93415378+Humanerer@users.noreply.github.com>
2022-01-22 01:14:27 +1100

    added attributes to towers for attack speed, damage, and range, added attack method which returns how much damage a tower does, or 0 if it is still on cooldown

--- Tower.java
+++ Tower.java
@@ -1,4 +1,8 @@
 public class Tower implements BoardPiece{
+    private int attackRadius = 1;
+    private int attackSpeed = 1;
+    private int attackCoolDown = 0;
+    private int damage = 1;
 
     @Override
     public void tick() {
@@ -8,6 +12,19 @@ public class Tower implements BoardPiece{
     @Override
     public char asChar() {
         return 'T';
+    }
+
+    public int getAttackRadius(){
+        return attackRadius;
+    }
+
+    public int attack(){
+        if (attackCoolDown <= 0){
+            attackCoolDown = attackSpeed;
+            return damage;
+        }
+        attackCoolDown--;
+        return 0;
     }
 
 }
