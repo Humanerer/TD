7e5a5a68e0ccf5d52828afb2d58fae3ce85d0930
Allan Paul (s3849758) <93415378+Humanerer@users.noreply.github.com>
2022-02-12 18:15:30 +1100

    refactored magic numbers

--- SimpleTD.java
+++ SimpleTD.java
@@ -1,17 +1,22 @@
 import java.util.LinkedList;
 
 class SimpleTD {
+
+    final static int width = 32;
+    final static int height = 18;
+
     public static void main(String[] args) {
         int lives = 10;
+        final int frameDelayMs = 200;
 
         LinkedList<CoordVector> path = new LinkedList<CoordVector>();
         for (int x = 0; x < 32; x++) {
             path.add(new CoordVector(x,4));
         }
 
+        GameFrame gameFrame = new GameFrame(width, height);
 
-
-        Board board = new Board(32,18, path);
+        Board board = new Board(width, height, path);
         board.printBoard();
         System.out.println();
         board.placeTower(new Tower(4,5),4,5);
@@ -25,7 +30,7 @@ class SimpleTD {
                 board.printBoard();
                 System.out.println(lives);
 
-                Thread.sleep(1000);
+                Thread.sleep(frameDelayMs);
             }
             System.out.println("<< GAME OVER >>");
         } catch (Exception e){
